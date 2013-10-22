d8421946c5dad96853cee35e830c62d9f3ba92ec
Luke Wendling <luke@lukewendling.com>
2013-10-22 13:00:08 -0500

    use angular factory for links

--- public/javascripts/links.js
+++ public/javascripts/links.js
@@ -1,5 +1,6 @@
-function LinksCtrl($scope) {
-  $scope.links = {
+var resumeApp = angular.module('resumeApp', []); // no dependencies
+resumeApp.factory('Links', function () {
+  return {
     resume: {name: 'Resume', url: 'https://docs.google.com/document/d/1WvzCd7QYhnVWc1_tXVAqkRNaCPkBWk_5N19fnL0kQvY/pub'},
     github: {name: 'GitHub', url: 'https://github.com/lukewendling?tab=repositories'},
     linkedin: {name: 'LinkedIn', url: 'http://linkedin.com/in/lukewendling'},
@@ -7,4 +8,8 @@ function LinksCtrl($scope) {
     cover: {name: 'Cover', url: 'https://docs.google.com/document/d/1vT4-6lga_mP_UNJeb8hYGbgslIy3SSPX0mgEYjiddI4/pub'},
     elevator: {name: 'Elevator', url: 'https://github.com/lukewendling/yolo-octo-wookie'}
   };
+});
+
+function LinksCtrl($scope, Links) {
+  $scope.links = Links;
 }
