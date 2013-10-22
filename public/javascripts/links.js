b985ad4f5284ff9b958d1b8e1dc4eb9d3fc97370
Luke Wendling <luke@lukewendling.com>
2013-10-22 12:22:18 -0500

    mv links to angular.js data store

---
+++ public/javascripts/links.js
@@ -0,0 +1,10 @@
+function LinksCtrl($scope) {
+  $scope.links = {
+    resume: {name: 'Resume', url: 'https://docs.google.com/document/d/1WvzCd7QYhnVWc1_tXVAqkRNaCPkBWk_5N19fnL0kQvY/pub'},
+    github: {name: 'GitHub', url: 'https://github.com/lukewendling?tab=repositories'},
+    linkedin: {name: 'LinkedIn', url: 'http://linkedin.com/in/lukewendling'},
+    stats: {name: 'One Month', url: 'http://daytum.com/lukewendling'},
+    cover: {name: 'Cover', url: 'https://docs.google.com/document/d/1vT4-6lga_mP_UNJeb8hYGbgslIy3SSPX0mgEYjiddI4/pub'},
+    elevator: {name: 'Elevator', url: 'https://github.com/lukewendling/yolo-octo-wookie'}
+  };
+}
