1f7bddb57e72087c336904f5bbc0f56cbdf38c7e
Luke Wendling <luke@lukewendling.com>
2013-10-22 11:32:54 -0500

    upgrade Ruby, sinatra, thin, Readme

---
+++ README.md
@@ -0,0 +1,12 @@
+Requirements
+------------
+
+ruby 1.9.3
+
+Install
+-------
+
+```
+bundle
+ruby app.js
+```
--- heroku-sinatra-app.rb
+++ heroku-sinatra-app.rb
@@ -18,8 +18,7 @@ end
 
 # Quick test
 get '/' do
-  "Congradulations!
-   You're running a Sinatra application on Heroku!"
+  "You're running a Sinatra application on Heroku!"
 end
 
 # Test at <appname>.heroku.com
